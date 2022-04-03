f8a6787312d2b2343bc6d50b8e3cf53e259ef00c
Studio Kaori <studiokaori.com@gmail.com>
2022-04-03 08:50:04 +0200

    Initial Commit

---
+++ MathTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathTraining
+//
+//  Created by Kaori Persson on 2022-04-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
