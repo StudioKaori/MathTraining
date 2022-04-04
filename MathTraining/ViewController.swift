b7581fd30821094ab0e63d72f02968433a1d8386
Studio Kaori <studiokaori.com@gmail.com>
2022-04-04 13:55:50 +0200

    Add: ResultViewController for showing the quiz result

--- MathTraining/ViewController.swift
+++ MathTraining/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func backToTitle(sender: UIStoryboardSegue){
+        // Just tag for back button in ResultView, keep it empty
+    }
 
 
 }
